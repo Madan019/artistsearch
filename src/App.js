3a096646c6281db1971f1ea1ca011a974cdb3f85
madan019 <madanlal141710@gmail.com>
2022-09-09 17:03:44 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
+function App () {
+  const CLIENT_ID = '6012b07d7d7742f9ae4e91d0c81ceeda'
+  const REDIRECT_URI = 'http://localhost:3000'
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+  const RESPONSE_TYPE = 'token'
+
+  const [token, setToken] = useState('')
+  const [searchKey, setSearchKey] = useState('')
+  const [artists, setArtists] = useState([])
+  // const [song, setSong] = useState([])
+
+  // const getToken = () => {
+  //     let urlParams = new URLSearchParams(window.location.hash.replace("#","?"));
+  //     let token = urlParams.get('access_token');
+  // }
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem('token')
+
+    // getToken()
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split('&')
+        .find(elem => elem.startsWith('access_token'))
+        .split('=')[1]
+
+      window.location.hash = ''
+      window.localStorage.setItem('token', token)
+    }
+
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken('')
+    window.localStorage.removeItem('token')
+  }
+
+  const searchArtists = async e => {
+    e.preventDefault()
+    const { data } = await axios.get('https://api.spotify.com/v1/search', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: 'artist'
+      }
+    })
+
+    setArtists(data.artists.items)
+    console.log(data.artists)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+      <div>
+        <div className='artistDisplay' key={artist.id}>
+          {artist.images.length ? (
+            <img className='images' src={artist.images[0].url} alt='' />
+          ) : (
+            <div>No Image</div>
+          )}
+          <div className='artistInfo'>
+            <h3 style={{ textTransform: 'capitalize' }}>{artist.type}</h3>
+            <p className='info'>
+              Name : {artist.name}
+              <br />
+              Followers : {artist.followers.total}
+              <br />
+              Popularity : {artist.popularity}
+            </p>
+          </div>
+        </div>
+      </div>
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Artist Search App</h1>
+        {!token ? (
+          <a
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <button onClick={logout} className='btn'>
+            Logout
+          </button>
+        )}
+
+        {token ? (
+          <form onSubmit={searchArtists}>
+            <div className='form'>
+              <input
+                className='inputName'
+                type='text'
+                onChange={e => setSearchKey(e.target.value)}
+              />
+              <button type={'submit'} className='btn'>
+                Search
+              </button>
+            </div>
+          </form>
+        ) : (
+          <h2>Please login</h2>
+        )}
+
+        {renderArtists()}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
